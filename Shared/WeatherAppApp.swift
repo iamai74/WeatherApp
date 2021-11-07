61ac58f1712a8f75d8663a9fa3b1dba3e984fec2
iamai <zhigunovep@intersvyaz.net>
2021-11-07 16:33:32 +0500

    Initial Commit

---
+++ Shared/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  Shared
+//
+//  Created by iamai on 07.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
